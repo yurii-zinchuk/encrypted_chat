22ddfbea58b6ea80ddf4b227524e7f8054e75a08
YuriiKhr <yurii.kharabara@ucu.edu.ua>
2022-06-05 22:53:04 +0300

    Example added

--- encryption/dsa.py
+++ encryption/dsa.py
@@ -1,5 +1,6 @@
 import random
 
+import time
 
 def DSA_encode(message, p, q, g, x):
     """
@@ -21,6 +22,7 @@ def DSA_encode(message, p, q, g, x):
 
 
 def modular_multiplicative_inverse(number, module):
+    """Finds modular multiplicative inverse"""
     number_inversed = 0
     while (number * number_inversed) % module != 1:
         number_inversed += 1
@@ -46,6 +48,14 @@ def DSA_check_signature(message, p, q, g, y, r, s):
 
 
 def encode(message):
+    """Creates signature for message
+
+    Args:
+        message (str): message which is going to be sent
+
+    Returns:
+        tuple: message and packet with signature
+    """
     p = 10091       # It should be bigger for better secure(e.g. 512 bit - 1024 bit) *prime number
     q = 1009        # p = q*k + 1 (p is also prime)
     for h in range(p):
@@ -61,28 +71,59 @@ def encode(message):
 
 
 def check_signature(message_packet):
-    message, p, q, g, y, r, s = message_packet
+    """Checks signature validity
+
+    Args:
+        message_packet (tuple): message and packet with signature
+
+    Returns:
+        str: message
+    """
+    message, (p, q, g, y, r, s) = message_packet
     if DSA_check_signature(message, p, q, g, y, r, s):
         return message
+    else:
+        return False
 
 
 def DSA_example():
-    message = 'Hello World'
-    q = 1009
-    p = 10091
-    for h in range(p):
-        if (h**((p-1)/q)) % p > 1:
-            g = int((h**((p-1)/q)) % p)
-            break
-    x = random.randint(0, q)  # private_key
-    y = (g**x) % p  # public_key
+    message = input('Please, type your message: ')
+    for i in range(12):
+        if i == 11:
+            print(f"\rencoding{'.'*(i%4)}    ")
+        else:
+            print(f"\rencoding{'.'*(i%4)}    ", end='')
+        time.sleep(0.2)
+    
+    message_packet = encode(message)
 
+    for i in range(12):
+        if i == 11:
+            print(f"\rsending packet{'.'*(i%4)}   ")
+        else:
+            print(f"\rsending packet{'.'*(i%4)}   ", end='')
+        time.sleep(0.2)
+
+    print('Message packet:', message_packet)
+    print("Encoded message signature:", message_packet[1][-2], message_packet[1][-1])
+    time.sleep(3)
     # the private key package is {p,q,g,x}.
     # The public key package is {p,q,g,y}.
+    print('\n\n(Another user)\nNow lets check signature validity')
+    
+    for i in range(12):
+        if i == 11:
+            print(f"\rChecking{'.'*(i%4)}   ")
+        else:
+            print(f"\rChecking{'.'*(i%4)}   ", end='')
+        time.sleep(0.2)
 
-    r, s = DSA_encode(message, p, q, g, x)
-    print("Encoded message signature:", r, s)
-    print("Check signature:", DSA_check_signature(message, p, q, g, y, r, s))
+    if check_signature(message_packet) is not False:
+        print("Signature is valid!")
+        print("Your message:", check_signature(message_packet))
+    else:
+        print('Signature is not valid!')
 
 
-DSA_example()
+if __name__=='__main__':
+    DSA_example()
