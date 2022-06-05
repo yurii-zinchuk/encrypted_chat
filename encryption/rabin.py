9caca677af82b762edcc964cdc6194468d1e0928
Sofiia-K <87498412+Sofiia-K@users.noreply.github.com>
2022-06-05 13:13:20 +0300

    Update rabin.py

--- encryption/rabin.py
+++ encryption/rabin.py
@@ -3,7 +3,89 @@ module with Rabin cryptography
 technique implementation
 """
 import math
-from functools import reduce
+from random import randint
+
+
+NUMS_DICT = {
+    "Q": "10",
+    "W": "11",
+    "E": "12",
+    "R": "13",
+    "T": "14",
+    "Y": "15",
+    "U": "16",
+    "I": "17",
+    "O": "18",
+    "P": "19",
+    "A": "20",
+    "S": "21",
+    "D": "22",
+    "F": "23",
+    "G": "24",
+    "H": "25",
+    "J": "26",
+    "K": "27",
+    "L": "28",
+    "Z": "29",
+    "X": "30",
+    "C": "31",
+    "V": "32",
+    "B": "33",
+    "N": "34",
+    "M": "35",
+    "q": "36",
+    "w": "37",
+    "e": "38",
+    "r": "39",
+    "t": "40",
+    "y": "41",
+    "u": "42",
+    "i": "43",
+    "o": "44",
+    "p": "45",
+    "a": "46",
+    "s": "47",
+    "d": "48",
+    "f": "49",
+    "g": "50",
+    "h": "51",
+    "j": "52",
+    "k": "53",
+    "l": "54",
+    "z": "55",
+    "x": "56",
+    "c": "57",
+    "v": "58",
+    "b": "59",
+    "n": "60",
+    "m": "61",
+    " ": "62",
+    ",": "63",
+    ".": "64",
+    "!": "65",
+    "?": "66",
+    "-": "67",
+    "`": "68",
+    "'": "69",
+    '"': "70",
+    ";": "71",
+    ":": "72",
+    "(": "73",
+    ")": "74",
+    "/": "75",
+    "|": "76",
+    "_": "77",
+    "1": "78",
+    "2": "79",
+    "3": "80",
+    "4": "81",
+    "5": "82",
+    "6": "83",
+    "7": "84",
+    "8": "85",
+    "9": "86",
+    "0": "87",
+}
 
 
 def keys_generation():
@@ -26,7 +108,7 @@ def keys_generation():
         'checks if the numbers are coprime'
         return math.gcd(a, b) == 1
 
-    def p_q_gener(the_range=(6,15)):
+    def p_q_gener(the_range=(8,20)):
         'generates p and q values'
         for elem in range(the_range[0], the_range[1]):
             if is_prime(elem) and elem % 4 == 3:
@@ -49,94 +131,76 @@ def keys_generation():
 
 
 
-def encrypt(n, message):
+def encrypt(n, message, server):
     'encrypting the message'
 
-    def message_to_ascii(message):
-        """turns message to ASCII,
-        then to binary"""
-        # right now, use just an ordinary number
+    def get_id():
+        'gets the id for current operation'
+        id = randint(1, 1000000000000000)
+        return id
+
+    def message_to_num(message):
+        """turns message to num"""
+        num = int(NUMS_DICT[str(message)])
         # return m
-        return message
+        return num
 
     def formula_encryption(m, n):
         'formula for encryption'
         c = m**2 % n
         return c
 
-    # message = message_to_ascii(message)
-    encr = formula_encryption(message_to_ascii(message), n)
-    return encr
+    id_val = get_id()
+    server.register(message, id_val)
+    the_num = message_to_num(message)
+    encr = formula_encryption(the_num, n)
+    return encr, id_val
 
-def decrypt(c, p, q):
+def decrypt(c, p, q, server):
     'decrypting the message'
 
-# function that implements Extended euclidean
-# algorithm
-    def extended_euclidean(a, b):
-        if a == 0:
-            return (b, 0, 1)
+
+    def eucl_alg(val1, val2):
+        'recursive euclidian algorithm'
+        if val1 == 0:
+            return (val2, 0, 1)
         else:
-            g, y, x = extended_euclidean(b % a, a)
-            return (g, x - (b // a) * y, y)
+            st1_val, st2_val, st3_val = eucl_alg(val2 % val1, val1)
+            val3 = st3_val - (val2 // val1) * st2_val
+            return (st1_val, val3, st2_val)
+
+
+    def inverse(val1, val2):
+        'finds the modular inverse number'
+        st1_val, st2_val, st3_val = eucl_alg(val1, val2)
+        return st2_val % val2
     
-    # modular inverse driver function
-    def modinv(a, m):
-        g, x, y = extended_euclidean(a, m)
-        return x % m
     
-    # function implementing Chinese remainder theorem
-    # list m contains all the modulii
-    # list x contains the remainders of the equations
-    def crt(m, x):
-    
-        # We run this loop while the list of
-        # remainders has length greater than 1
+    def chinese_remainder_count(val1:list, val2:list):
+        'chinese remainder theorem for calculations'
         while True:
-            
-            # temp1 will contain the new value
-            # of A. which is calculated according
-            # to the equation m1' * m1 * x0 + m0'
-            # * m0 * x1
-            temp1 = modinv(m[1],m[0]) * x[0] * m[1] + \
-                    modinv(m[0],m[1]) * x[1] * m[0]
-    
-            # temp2 contains the value of the modulus
-            # in the new equation, which will be the
-            # product of the modulii of the two
-            # equations that we are combining
-            temp2 = m[0] * m[1]
-    
-            # we then remove the first two elements
-            # from the list of remainders, and replace
-            # it with the remainder value, which will
-            # be temp1 % temp2
-            x.remove(x[0])
-            x.remove(x[0])
-            x = [temp1 % temp2] + x
-    
-            # we then remove the first two values from
-            # the list of modulii as we no longer require
-            # them and simply replace them with the new
-            # modulii that  we calculated
-            m.remove(m[0])
-            m.remove(m[0])
-            m = [temp2] + m
-    
-            # once the list has only one element left,
-            # we can break as it will only  contain
-            # the value of our final remainder
-            if len(x) == 1:
+            inv1, inv2 = inverse(val1[1],val1[0]), inverse(val1[0],val1[1])
+            st1_def1 = inv1 * val2[0] * val1[1]
+            st2_def2 = inv2 * val2[1] * val1[0]
+            st1 = st1_def1 + st2_def2
+            st2 = val1[0] * val1[1]
+            # val1_2' * val1_2 * val2_1 + val1_1' * val1_1 * val2_2
+            # modify val2 and val1 lists
+            val2.remove(val2[0])
+            val2.remove(val2[0])
+            val1.remove(val1[0])
+            val1.remove(val1[0])
+            val1, val2 = [st2] + val1, [st1 % st2] + val2
+            # if only one elem left - break
+            if len(val2) == 1:
                 break
-    
-        # returns the remainder of the final equation
-        return x[0]
+        return val2[0]
 
     def chinese_theorem(tuple1, tuple2):
         'chinese theorem for 2 components'
         mod_num_ls = [tuple1[1], tuple2[1]]
         gen_num_ls = [tuple1[0], tuple2[0]]
-        result = crt(mod_num_ls, gen_num_ls)
+        result = chinese_remainder_count(mod_num_ls, gen_num_ls)
         return result
 
     def normal_form(tuple_val):
@@ -179,50 +243,101 @@ def decrypt(c, p, q):
 
         return [result1, result2, result3, result4]
     
-    ls = define_answ(c, p, q)
+    ls = define_answ(int(c), p, q)
     return ls
 
 
-class Datum:
+class ServerData:
     """class for a final set of values;
     is used to verify them on server"""
-    def __init__(self, pt1, pt2, pt3, pt4) -> None:
-        self.pt1 = pt1
-        self.pt2 = pt2
-        self.pt3 = pt3
-        self.pt4 = pt4
+    def __init__(self) -> None:
+        self.dct = dict()
+
+    def register(self, datum, id):
+        'registers the datum on the server side'
+        self.dct[id] = datum
+
+    def check_data(self, data, id):
+        'checks the final data'
+        if self.dct[id] in data:
+            return self.dct[id]
 
 
-def get_input(n):
+
+def get_input(n, server):
     'gets input from the user'
     message = input('please, write your message:   ')
     to_send = ''
+    id_ls = []
     for symb in message:
-        enc = encrypt(n, ord(symb))
-        to_send = to_send + '_' + str(enc)
-    return to_send
+        enc, id_val = encrypt(n, symb, server)
+        id_ls.append(id_val)
+        add_str = '*' + str(enc)
+        to_send += add_str
+    to_send = to_send[1:]
+    return to_send, id_ls
 
-def get_data(data, p, q):
+def get_data(data, p, q, server, nums_dict, id_ls):
     'gets the data and decrypts it'
-    to_check = []
-    to_decrypt = data.split('_')
-    for datum in to_decrypt:
-        result = decrypt(datum, p, q)
-        to_check.append(result)
-    return to_check
+    final_result = ''
+    to_decrypt = data.split('*')
+    for idx, datum in enumerate(to_decrypt):
+        id_val = id_ls[idx]
+        decr = decrypt(datum, p, q, server)
+        # to_check.append(result)
 
+        
+        answ_ls = []
+        for el in decr:
+            try:
+                check_dict = {i: j for j, i in nums_dict.items()}
+                elem = check_dict[str(el)]
+                answ_ls.append(elem)
+            except:
+                pass
+
+        result = server.check_data(answ_ls, id_val)
+        # print(result)
+        final_result += result
+    return final_result
 
 
 
 
 if __name__ == '__main__':
+    # ------ single character encoding/decoding test ------\
+
+    # server = ServerData()
+    # n, (p, q) = keys_generation()
+    # # n, (p, q) = 161, (23, 7)
+
+    # print(f'n:{n}, p, q:{p},{q}')
+
+    # message = 'd'
+    # encr, id_val = encrypt(n, message, server)
+    # print(encr, '  encr')
+    # decr = decrypt(encr, p, q, server)
+    # print(decr, '   chinese theorem decr')
+    # answ_ls = []
+    # for el in decr:
+    #     try:
+    #         check_dict = {i: j for j, i in NUMS_DICT.items()}
+    #         elem = check_dict[str(el)]
+    #         answ_ls.append(elem)
+    #     except:
+    #         pass
+
+    # result = server.check_data(answ_ls, id_val)
+    # print(result)
+
+
+
+
+
+    # words encoding/decoding test
+    server = ServerData()
     n, (p, q) = keys_generation()
-    # n, (p, q) = 161, (23, 7)
+    to_send, id_ls = get_input(n, server)
+    final_result = get_data(to_send, p, q, server, NUMS_DICT, id_ls)
+    print(final_result)
 
-    print(f'n:{n}, p, q:{p},{q}')
-
-    message = 24
-    encr = encrypt(n, message)
-    print(encr, '  encr')
-    decr = decrypt(encr, p, q)  
-    print(decr, '   final decr')
